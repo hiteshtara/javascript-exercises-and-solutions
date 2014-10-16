1884dcdc3b9096e798c56461a64e21227f4762c0
FrancoAguilera <flaalf@gmail.com>
2014-10-16 12:12:25 -0300

    Nitpicking about alphabetizing methods, minor OCD thing

--- exercises/js/utils.js
+++ exercises/js/utils.js
@@ -1,1 +1,30 @@
 // TODO implement utils module
+var utils = {};
+
+utils.bind = function(){};
+
+utils.extend = function(){
+
+  var obj = {};
+
+  for(var i = 0; i < arguments.length; i++){
+
+    for(var key in arguments[i]){
+
+      if(typeof arguments[i] === "object" && !obj.hasOwnProperty(key)){
+
+        obj[key] = arguments[i][key];
+
+      }
+
+    }
+
+  }
+
+  return obj;
+
+};
+
+utils.memorize = function(){};
+
+utils.lazy = function(){};
