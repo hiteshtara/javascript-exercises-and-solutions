b387dab496f5d8bf7908a471f1c54ba34c7e35da
FrancoAguilera <flaalf@gmail.com>
2014-10-19 21:11:17 -0300

    That's just how I roll

--- exercises/js/async-promise.js
+++ exercises/js/async-promise.js
@@ -1,29 +1,41 @@
 // Dummy HTTP Mocks
 var q = require('../bower_components/q/q');
 var getProgramsMock = function() {
-  setTimeout(function() {
-    return ['Program 1', 'Program 2', 'Program 3'];
-  }, 1000);
+	setTimeout(function() {
+		return ['Program 1', 'Program 2', 'Program 3'];
+	}, 1000);
 };
 
 var scheduleProgramMock = function(callback) {
-  setTimeout(function() {
-    callback(true);
-  }, 1000);
+	setTimeout(function() {
+		callback(true);
+	}, 1000);
 };
 
 // TODO convert callback API from async-callback to a promise API implementation
 var Programs = {
 
-	get: q.fcall(getProgramsMock)
+	get: function () {
+		q.fcall(getProgramsMock).then(function (resolve, reject) {
+
+			return resolve // ????
+
+		});
+	}
+
+//	schedule: function() {
+//
+//		return q.fcall(scheduleProgramMock)
+//
+//	}
 
 };
 
 
 // Client code
 Programs.get().then(function(programs) {
-    console.log('Received', programs);
-  });
+	console.log('Received', programs);
+});
 
 //Programs.schedule('someProgramId').then(function(scheduled) {
 //    console.log('Schedule result', scheduled);
